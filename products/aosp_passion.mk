365c02213980186f9fc87de2c509a74a8476d2ba
macsa <y>
2011-03-02 16:02:54 +0100

    Nuevo kernel con havs, compila ADWLauncher y subida version
    Change-Id: I5334ddd1187d42cf7fc1cbeaed37116d981440b4

--- products/aosp_passion.mk
+++ products/aosp_passion.mk
@@ -21,9 +21,9 @@ PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_ID=GRI40 BUILD_DISPLAY_ID=GRI40 BUILD_FINGERPRINT=google/passion/passion/mahimahi:2.3.3/GRI40/102588:user/release-keys PRIVATE_BUILD_DESC="passion-user 2.3.3 GRI40 102588 release-keys"
 
 # Build kernel
-#PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
-#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=aosp-kernel
-#PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=aosp_defconfig
+PRODUCT_SPECIFIC_DEFINES += TARGET_PREBUILT_KERNEL=
+PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_DIR=/media/COMPILER/mydroid/Kernels/aosp-kernel
+PRODUCT_SPECIFIC_DEFINES += TARGET_KERNEL_CONFIG=aosp_mahimahi_defconfig
 
 # Extra Passion overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/macsa/overlay/passion
@@ -38,7 +38,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Set ro.modversion
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=SuperAosp-ST@-Nexus-2.2
+    ro.modversion=SuperAosp-ST@-Nexus-2.6
 
 #
 # Copy Passion specific prebuilt files
