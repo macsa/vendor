af8617af4bc62caf6d9d91e2c112be14080612ae
macsa <y>
2011-03-08 08:31:48 +0100

    Actualizados Google Apps
    Change-Id: Ied32893154fca7365eb34d0abf60c42dece7dfb1

--- products/aosp_geeksphone.mk
+++ products/aosp_geeksphone.mk
@@ -46,16 +46,4 @@ PRODUCT_COPY_FILES +=  \
 	vendor/macsa/prebuilt/one/etc/super2/03gps:system/etc/super2/03gps \
 	vendor/macsa/prebuilt/one/etc/super2/04control:system/etc/super2/04control
 
-PRODUCT_LOCALES := \
-    es_ES \
-    ca_ES \
-    en_US \
-    de_DE \
-    eu_ES \
-    fr_FR \
-    it_IT \
-    pt_PT \
-    ru_RU \
-    mdpi
-
-PRODUCT_DEFAULT_LANGUAGE := es_ES
+PRODUCT_LOCALES := es_ES ca_ES en_US de_DE eu_ES fr_FR it_IT pt_PT ru_RU mdpi
