63a8c06647dcb65beb6c1e173fb0efc397fdf89e
macsa <y>
2011-04-26 09:32:23 +0200

    Desactivado sonido de inicio y subido version
    Change-Id: I137ca258b81ef79e4dca05116bab06e83a9f7317

--- products/aosp_crespo.mk
+++ products/aosp_crespo.mk
@@ -37,8 +37,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Set ro.modversion
 #
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=SuperAosp-ST@-Nexus-S-6.2 \
-    ro.stats.romversion=6.2
+    ro.modversion=SuperAosp-ST@-Nexus-S-6.4 \
+    ro.stats.romversion=6.4
 #
 # Copy Nexus S specific prebuilt files
 #
