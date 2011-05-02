2b5faa5c455981c73142dbdcc86fcd790d81189a
macsa <y>
2011-05-02 22:00:20 +0200

    Editados scripts de instalacion
    Change-Id: Icb96367e6b62f1a6d0536fad4f4ab8eeab6c53a1

--- products/themes.mk
+++ products/themes.mk
@@ -2,7 +2,9 @@
 PRODUCT_PACKAGES += \
        Androidian \
        BatCircular \
+       BatCircularNelo \
        BatCircular2 \
+       BatNelo \
        BatNormal \
        BatNumerada \
        Cyanbread
