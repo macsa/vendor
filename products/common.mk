ec89bf4d9fdab79dec160a89d13068efb740456b
macsa <y>
2011-05-17 10:48:48 +0200

    Sonido en inicio de rom desactivado por defecto
    Change-Id: I029fe368788e312674593fdb3f46c8991826c15c

--- products/common.mk
+++ products/common.mk
@@ -15,7 +15,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.config.play.bootsound=1 \
+    ro.config.play.bootsound=0 \
     ro.stats.devname=SuperTe@m \
     ro.stats.romname=SuperAosp
 
